e3f0845eeb4daaf4b0b029f4476ce0d8cde1382d
Alec Puente <icepuente@gmail.com>
2016-04-10 00:36:34 -0700

    pep8ify code

--- llnw_assignment.py
+++ llnw_assignment.py
@@ -2,12 +2,12 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-dict = {'id' : 1, 'name' : 'Limelight'}
+dict = {'id': 1, 'name': 'Limelight'}
+
+
 @app.route('/')
 def dict_json():
-	return jsonify(**dict)
+    return jsonify(**dict)
 
 if __name__ == '__main__':
-	app.run()
-
-
+    app.run()
