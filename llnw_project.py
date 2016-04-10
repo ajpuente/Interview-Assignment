72ad1600d3079a3fc3309ef6eb74f1667b04f96d
Alec Puente <icepuente@gmail.com>
2016-04-09 17:41:09 -0700

    initial commit

---
+++ llnw_project.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+dict = {'id' : 1, 'name' : 'Limelight'}
+@app.route('/')
+def dict_json():
+	return jsonify(**dict)
+
+if __name__ == '__main__':
+	app.run()
+
+
